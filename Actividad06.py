21ad47b8e0d322fed1ae34b204fb8b7b1c027be8
Xantonio30111 <114781696+Xantonio30111@users.noreply.github.com>
2025-07-24 10:16:06 -0600

    Mejorando cosas, probando otras y poquito avance

--- Actividad06.py
+++ Actividad06.py
@@ -1,4 +1,11 @@
 listaNumeros = []
+calificaciones = []
+def pedirDatos ():
+    pedirNum = int(input("Ingrese la cantidad de numeros que quiere ingresar: "))
+    for i in range(pedirNum):
+        numero = int(input("Ingrese el numero: "))
+        listaNumeros.append(numero)
+
 def suma(numeros):
     suma = 0
     for i in numeros:
@@ -18,10 +25,15 @@ def positivoNegativo (numeros):
             negativos += 1
     return print(f"La cantidad de numeros positivos es: {positivos} y de negativos: {negativos}")
 
-def ares(a,b):
+def area(a,b):
     area = (a*b)/2
     return print(f"El area del triangulo es: {area}")
 
+def par(num):
+    if num%2==0:
+        print("Es par")
+    else:
+        print("Es impar")
 while True:
     print("--Bienvenido al Menu--")
     print("1. Suma, promedio y la cantidad de numeros positivos y negativos")
@@ -43,4 +55,14 @@ while True:
         case "2":
             altura= int(input("Ingrese la altura del triangulo: "))
             base = int(input("Ingrese la base del triangulo: "))
-            ares(altura, base)
+            area(altura, base)
+        case"3":
+            numero= int(input("Ingrese el numero que quiere evaluar"))
+            par(numero)
+        case "4":
+            pedirCali = int(input("Ingrese la cantidad de calificaciones que quiere ingresar: "))
+            for i in range(pedirCali):
+                calificacion = int(input("Ingrese sus calificaciones: "))
+                calificaciones.append(calificacion)
+            print(f"El promedio de sus calificaciones: {promedio(calificaciones)}")
+
