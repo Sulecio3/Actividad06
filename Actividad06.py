fc9cf64e4a4c27b0e6af94b9faf3ea8b7e121502
Xantonio30111 <114781696+Xantonio30111@users.noreply.github.com>
2025-07-24 09:52:14 -0600

    Avance 2, ya se corrigieron errores y se avanzo mas

--- Actividad06.py
+++ Actividad06.py
@@ -1,18 +1,39 @@
 listaNumeros = []
-def suma(listaNumeros):
-    for i in listaNumeros:
-        listaNumeros += 1
-    return listaNumeros
+def suma(numeros):
+    suma = 0
+    for i in numeros:
+        suma += i
+    return suma
 
-def promedio(ListaNumeros):
-    return suma(listaNumeros)/len(listaNumeros)
+def promedio(numeros):
+    return suma(numeros)/len(numeros)
+
+def positivoNegativo (numeros):
+    positivos = 0
+    negativos = 0
+    for i in numeros:
+        if i > 0:
+            positivos +=1
+        else:
+            negativos += 1
+    return print(f"La cantidad de numeros positivos es: {positivos} y de negativos: {negativos}")
 
 
 while True:
-    option = input("ingrese la opcion en la que quiera entrar")
+    print("--Bienvenido al Menu--")
+    print("1. Suma, promedio y la cantidad de numeros positivos y negativos")
+    print("2. Area de un triangulo")
+    print("3. Verificar si el numero es par o impar")
+    print("4. calcular el promedio")
+    print("5. Mostrar cual es el mayor y menor")
+    print("6. Salir")
+    option = input("ingrese la opcion en la que quiera entrar: ")
     match option:
         case "1":
-            pedirNum = int(input("Ingrese la cantidad de numeros que quiere ingresar"))
-            for i in pedirNum:
-                numero = int(input("Ingrese el numero"))
+            pedirNum = int(input("Ingrese la cantidad de numeros que quiere ingresar: "))
+            for i in range(pedirNum):
+                numero = int(input("Ingrese el numero: "))
                 listaNumeros.append(numero)
+            print(f"La suma de todos sus numeros es: {suma(listaNumeros)}")
+            print(f"El promedio de todos sus numeros es: {promedio(listaNumeros)}")
+            positivoNegativo(listaNumeros)
